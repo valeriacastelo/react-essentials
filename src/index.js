f649fd01d34242e1ea477ef6ca90a8d6d389dad4
valeriacastelobranco <valeriacastelobranco@gmail.com>
2021-08-11 12:11:12 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  React.createElement(
+    "h1", 
+    {style: {color: "blue"}},
+    "Hello World React"
+  ),
+  document.getElementById('root')
+);
+
