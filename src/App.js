df5d36614125f3354e533f455a6cf29166e00f2e
valeriacastelobranco <valeriacastelobranco@gmail.com>
2021-08-30 15:36:23 +0100

    adding tests with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,56 +1,28 @@
 import './App.css';
-import restaurant from "./restaurant.jpg"
+import React, {useState, useEffect} from 'react';
 
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.name}'s Kitchen</h1>
-    </header>
-  )
-}
 
-function Main(props) {
-  return (
-    <section>
-      <p>We serve the most {props.adjetive} food around !!</p>
-
-      <img src={restaurant} 
-           height={150} 
-           alt="napkin and fork on the table"/>
-
-      <ul style={{textAlign : "left"}}>
-        { props.dishes.map((dish) => 
-            <li key={dish.id}>{dish.title}</li>
-        )}
-      </ul>
-    </section>
-  )
-}
-
-function Footer(props) {
-  return (
-    <footer>
-      <p>Copyright {props.year}</p>
-    </footer>
-  )
-}
-
-const dishes = [
-  "Macaroni",
-  "Salmon",
-  "Tofu with Vegetebles",
-  "Fish and Fries"
-]
-
-const dishesObj = dishes.map((dish, i) => ({ id: i, title: dish }));
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect (() => {
+    console.log(`It's ${emotion} here`);
+  }, [emotion]);
+
+  useEffect (() => {
+    console.log(`It's ${secondary} here`);
+  }, [secondary]);
+
   return (
-    <div className="App">
-      <Header name="Cindy"/>
-      <Main adjetive="amazing" dishes={dishesObj}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
+    <>
+      <h1>Current emotion is {emotion} and {secondary}</h1>
+      <button onClick={() => setEmotion("happy")}>Make Happy</button>
+      <button onClick={() => setSecondary("crabby")}>Make crabby</button>
+      <button onClick={() => setEmotion("fustrated")}>Frustate</button>
+      <button onClick={() => setEmotion("enthusiastic")}>Enthese</button>
+    </>
   )
 }
 
